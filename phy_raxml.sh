c6066e45324a09471fe575e900cd3ec32b0d1292
xuwanxing <75006906+xuwanxing@users.noreply.github.com>
2021-11-26 10:52:23 +0800

    Update phy_raxml.sh

--- phy_raxml.sh
+++ phy_raxml.sh
@@ -1,3 +1,4 @@
-#sed 's/*/-/g' Combine_all_geno_filtered_final_snp.min4.phy > Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy
+# to remove "*"
+sed 's/*/-/g' Combine_all_geno_filtered_final_snp.min4.phy > Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy
 
 raxmlHPC -x 12345 -p 12345 -# 1000 -m GTRCAT -s Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy -f a -n raxml_result -T 20
