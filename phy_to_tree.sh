d0a1e18f9b2c748082dd3e6b25625bab09f8990e
xuwanxing <75006906+xuwanxing@users.noreply.github.com>
2022-01-04 16:26:43 +0800

    Update and rename phy_raxml.sh to phy_to_tree.sh

--- phy_to_tree.sh
+++ phy_to_tree.sh
@@ -1,4 +1,10 @@
 # to remove "*"
 sed 's/*/-/g' Combine_all_geno_filtered_final_snp.min4.phy > Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy
 
+#raxml or raxml-ng (long time)
 raxmlHPC -x 12345 -p 12345 -# 1000 -m GTRCAT -s Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy -f a -n raxml_result -T 20
+
+#iqtree (recommended)
+iqtree -s Combine_all_geno_filtered_final_snp_replace_asterisk.min4.phy -m GTR -bb 1000 -bnni -nt AUTO -redo
+
+#both raxml and iqtree can only use one-node compute resource, even give resource of more than one-node
